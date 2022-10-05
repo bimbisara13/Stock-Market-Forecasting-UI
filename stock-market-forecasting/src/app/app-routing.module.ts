ab5e11db14ee1b265cb30d24eb1b7cb4beda2028
bimbisara13 <bimbisara13@gmail.com>
2022-10-04 17:15:32 -0700

    New: added routing

--- stock-market-forecasting/src/app/app-routing.module.ts
+++ stock-market-forecasting/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomePageComponent },
+  { path: 'main', component: MainPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
+export const routingComponents = [HomePageComponent, MainPageComponent]
