7d5d6851315d758782d38301a9043282b5ff944b
bimbisara13 <bimbisara13@gmail.com>
2022-10-05 19:23:55 -0700

    New: main-page draft

---
+++ stock-market-forecasting/src/app/main-page/main-page-interface.ts
@@ -0,0 +1,4 @@
+export interface Stocks {
+  value: string;
+  viewValue: string;
+}
--- stock-market-forecasting/src/app/main-page/main-page.component.ts
+++ stock-market-forecasting/src/app/main-page/main-page.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Stocks } from './main-page-interface';
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
+
 export class MainPageComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  stocks: Stocks[] = [
+    {value: 'tcs', viewValue: 'TCS'},
+    {value: 'adobe', viewValue: 'Adobe Inc.'},
+    {value: 'salesforce', viewValue: 'Salesforce, Inc.'},
+    {value: 'boeing', viewValue: 'The Boeing Company'},
+    {value: 'tesla', viewValue: 'Tesla, Inc.'},
+    {value: 'nvidia', viewValue: 'NVIDIA Corporation'},
+    {value: 'pfizer', viewValue: 'Pfizer, Inc. Common Stock'},
+    {value: 'jpmorgan', viewValue: 'JP Morgan Chase & Co.'},
+  ];
 
 }
