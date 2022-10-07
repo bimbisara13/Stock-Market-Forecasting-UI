2eca53959fba2eb8e28ba3b9479baea72d2491f9
bimbisara13 <bimbisara13@gmail.com>
2022-10-06 19:33:56 -0700

    New: analysis component

---
+++ stock-market-forecasting/src/app/analysis/analysis.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { StocksService } from '../stocks.service';
+
+@Component({
+  selector: 'app-analysis',
+  templateUrl: './analysis.component.html',
+  styleUrls: ['./analysis.component.css'],
+})
+export class AnalysisComponent implements OnInit {
+  stock: any;
+  stockId: any;
+
+  constructor(private activatedRoute: ActivatedRoute, private service: StocksService) {}
+
+  ngOnInit(): void {
+    this.stockId = this.activatedRoute.snapshot.paramMap.get('id');
+    this.stock = this.service.stocks.find(x => x.id == this.stockId);
+  }
+}
